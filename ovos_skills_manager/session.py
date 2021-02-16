ce6e208adce9848929e26d7a35b72dc3c0eb06ad
jarbasal <jarbasai@mailfence.com>
2021-02-16 00:01:33 +0000

    user agent

--- ovos_skills_manager/session.py
+++ ovos_skills_manager/session.py
@@ -1,16 +1,18 @@
 import requests_cache
-
+USER_AGENT = "OSM v0.0.X"
 SESSION = requests_cache.CachedSession(expire_after=5 * 60, backend="memory")
+SESSION.headers = {"User-Agent": USER_AGENT}
 
 
 def set_github_token(token):
     global SESSION
-    SESSION.headers = {
-        "Accept": "application/vnd.github.v3.raw",
-        "Authorization": f"token {token}"
-    }
+    SESSION.headers["Accept"] = "application/vnd.github.v3.raw"
+    SESSION.headers["Authorization"] = f"token {token}"
 
 
 def clear_github_token():
     global SESSION
-    SESSION.headers = {}
+    if "Authorization" in SESSION.headers:
+        SESSION.headers.pop("Authorization")
+    if SESSION.headers.get("Accept", "") == "application/vnd.github.v3.raw":
+        SESSION.headers.pop("Accept")
