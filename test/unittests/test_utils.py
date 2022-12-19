ee78edf03e901f39af9d71ea6bcff08e32a0bc2f
Daniel McKnight <daniel@neongecko.com>
2022-12-19 14:20:52 -0800

    Migrate common skill util functions from neon_core to OSM

--- test/unittests/test_utils.py
+++ test/unittests/test_utils.py
@@ -4,15 +4,18 @@ import unittest
 
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
-class TestSkillEntry(unittest.TestCase):
+
+class TestUtils(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         if os.environ.get("GITHUB_TOKEN"):
             from ovos_skills_manager.session import set_github_token
             set_github_token(os.environ.get("GITHUB_TOKEN"))
 
-    def test_parse_python_dependencies_no_auth(self):
+    def test_parse_python_dependencies(self):
         from ovos_skills_manager.utils import parse_python_dependencies
+
+        # No Auth
         test_input = ["package~=0.1",
                       "package~=0.2",
                       "git_dep @ git+https://github.com/myUser/SecretDependency@Branch"]
@@ -21,8 +24,7 @@ class TestSkillEntry(unittest.TestCase):
                         "git_dep @ git+https://github.com/myuser/secretdependency@Branch"]
         self.assertEqual(parse_python_dependencies(test_input), valid_output)
 
-    def test_parse_python_dependencies_with_auth(self):
-        from ovos_skills_manager.utils import parse_python_dependencies
+        # With Auth
         test_input = ["package~=0.1",
                       "package~=0.2",
                       "git_dep @ git+https://github.com/myUser/SecretDependency@Branch"]
@@ -31,7 +33,48 @@ class TestSkillEntry(unittest.TestCase):
                         "git_dep @ git+https://TOKEN@github.com/myuser/secretdependency@Branch"]
         self.assertEqual(parse_python_dependencies(test_input, "TOKEN"), valid_output)
 
-    # TODO: Test Readme/Desktop to json methods
+    def test_readme_to_json(self):
+        pass
+
+    def test_desktop_to_json(self):
+        pass
+
+    def test_build_skills_list(self):
+        pass
+
+    def test_read_skill_json(self):
+        pass
+
+    def test_read_skill_examples(self):
+        pass
+
+    def test_get_skills_info(self):
+        pass
+
+    def test_get_skills_examples(self):
+        pass
+
+    def test_get_skills_from_url(self):
+        from ovos_skills_manager.utils import get_skills_from_url
+        test_url = 'https://raw.githubusercontent.com/NeonGeckoCom/neon_skills/' \
+                   '606421bb3e9b3436ab3ca1a6bc51476f67e0e09a/' \
+                   'skill_lists/DEFAULT-SKILLS'
+        skills = get_skills_from_url(test_url)
+        self.assertIsInstance(skills, list)
+        self.assertTrue(all((x.startswith('https://github.com')
+                             for x in skills)))
+
+    def test_set_osm_constraints_file(self):
+        import ovos_skills_manager.requirements
+        from ovos_skills_manager.utils import set_osm_constraints_file
+        set_osm_constraints_file(__file__)
+        self.assertEqual(ovos_skills_manager.requirements.DEFAULT_CONSTRAINTS,
+                         __file__)
+
+    def test_get_pypi_version(self):
+        from ovos_skills_manager.utils import get_pypi_package_versions
+        versions = get_pypi_package_versions("neon-skill-about")
+        self.assertIsInstance(versions, list)
 
 
 if __name__ == '__main__':
