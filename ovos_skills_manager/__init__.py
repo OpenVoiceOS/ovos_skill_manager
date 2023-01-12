8e7ea872d0fcc886b2454461137fe1c97d6351dd
emphasize <swen_g@t-online.de>
2023-01-12 12:41:01 +0100

    remove forcing log level info (#105)

--- ovos_skills_manager/__init__.py
+++ ovos_skills_manager/__init__.py
@@ -7,7 +7,5 @@ from ovos_skills_manager.appstores.neon import NeonSkills
 from ovos_skills_manager.appstores.local import InstalledSkills
 from ovos_skills_manager.osm import OVOSSkillsManager
 from ovos_skills_manager.upgrade_osm import do_launch_version_checks
-from ovos_utils.log import LOG
 
-LOG.set_level("INFO")
 do_launch_version_checks()
